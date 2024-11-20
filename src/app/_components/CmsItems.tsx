49c10397afda8e4651d5829f5f647205cad10476
Luke <luke@lpdev.io>
2024-11-20 16:43:43 +1100

    chore: add boilerplate

---
+++ src/app/_components/CmsItems.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function CmsItems() {
+  return (
+    <div>
+      <h1>Items:</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import CmsItems from '@/components/CmsItems';
+
 export default function Home() {
   return (
-    <main className="flex min-h-[100svh] w-screen flex-col items-center justify-center">
-      <h1 className="text-xl">Homepage</h1>
+    <main>
+      <CmsItems />
     </main>
   );
 }
